e8380bcb9ea37dc0ab7fc3fd81a90244625fcfff
Dario Milicic <dario.milicic@gmail.com>
2015-12-10 20:29:57 +0100

    Basic dependencies and project structure setup

--- app/src/main/java/com/kodelabs/mycosts/presentation/activities/MainActivity.java
+++ app/src/main/java/com/kodelabs/mycosts/presentation/activities/MainActivity.java
@@ -1,13 +1,15 @@
-package com.kodelabs.mycosts;
+package com.kodelabs.mycosts.presentation.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.kodelabs.mycosts.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
